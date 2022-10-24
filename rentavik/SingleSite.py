2b2459d758404ab82a69bb811c9fdbb4e9294a24
ArtemSmirnovHSE <ar.smirnov2015@mail.ru>
2022-10-24 18:02:06 +0300

    Rentavik

---
+++ rentavik/SingleSite.py
@@ -0,0 +1,90 @@
+import requests
+import json
+from bs4 import BeautifulSoup, Comment
+import os
+from uuid import uuid4
+
+
+def parsePage(filename):
+    with open(f'./result/{filename}', 'r+', encoding='utf-8') as json_data:
+        rentavik_object_data = json.loads(json_data.read())
+
+        rentavik_object_link = rentavik_object_data['resource']['link']
+        print(rentavik_object_link)
+        page = requests.get(rentavik_object_link)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+        # ОПИСАНИЕ
+
+        paragraph_blacklist = [
+            'Пользовательское соглашение',
+            'Политика конфидициальности',
+            'Согласие на получение рекламных рассылок']
+        for suspect_p in soup.find_all('p'):
+            if suspect_p.get('href') is None and suspect_p.get('class') is None and suspect_p.string is not None\
+                    and not (suspect_p.string in paragraph_blacklist):
+                rentavik_object_data['realEstate']['description'] = suspect_p.string
+                break
+
+        # ДЕТАЛИ
+        details_suspects = soup.find_all('div', attrs={"class": "cell"})
+        for i in range(len(details_suspects)):
+            detail_key = details_suspects[i].find(
+                'div', attrs={'class': 'page-table__label'})
+            if detail_key is not None:
+
+                detail_value = details_suspects[i + 1].find(
+                    'div', attrs={'class': 'page-table__value'})
+                if detail_value is not None:
+                    detail_key = detail_key.string
+                    detail_key = detail_key.replace(
+                        '\r', '').replace('\n', '').strip()
+
+                    detail_value = detail_value.string
+                    detail_value = detail_value.replace(
+                        '\r', '').replace('\n', '').strip()
+                    rentavik_object_data['details'][detail_key] = detail_value
+
+        # ПИКЧИ
+        slides_tag = [
+            slides.find(
+                'div', attrs={
+                    'class': 'clients-slide__image'}) for slides in soup.find_all(
+                'div', attrs={
+                    "class": "clients-slide"})]
+        for slide_tag in slides_tag:
+            try:
+                img_source = slide_tag.find('img')['src']
+                img_link = 'https://www.rentavik.ru/' + img_source
+                rentavik_object_data['photo_link'].append(img_link)
+            except BaseException:
+                continue
+
+        # ЦЕНА
+        for price_info in soup.find_all(
+            'div', attrs={
+                'class': 'filterresults-offer-prices__value'}):
+            price_text = " ".join(
+                price_info.find_all(
+                    text=lambda t: not isinstance(
+                        t, Comment)))
+            price_text = price_text.replace('\n', '')
+            if 'руб./м 2  в год' in price_text:
+                price_text = price_text.replace(
+                    'руб./м 2  в год',
+                    '').replace(
+                    ' ',
+                    '')
+                rentavik_object_data['realEstate']['pricePerSquareMeterYear'] = int(
+                    price_text)
+            if 'руб./мес' in price_text:
+                price_text = price_text.replace(
+                    'руб./мес', '').replace(' ', '')
+                rentavik_object_data['realEstate']['pricePerMonth'] = int(
+                    price_text)
+
+        json_data_update = json.dumps(
+            rentavik_object_data, ensure_ascii=False, indent=4)
+        json_data.seek(0)
+        print(json_data_update)
+        json_data.write(json_data_update)
---
+++ rentavik/main.py
@@ -0,0 +1,130 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+from uuid import uuid4
+import os
+from SingleSite import parsePage # функция для парсинга страницы
+from progressbar import ProgressBar
+
+pbar = ProgressBar()
+
+
+# ID проекта
+rentavik_id = 'c110ffa55d6a43eeb1a12da21aac44c9'
+
+
+# создаем папку куда суем результаты
+if not os.path.exists('./result'):
+    os.makedirs('./result')
+
+# структура файла с основной информацией
+rentavik_object_data = {
+    "id": "",
+    "resource": {
+
+        "title": "rentavik",
+        "link": "",
+    },
+    "realEstate": {
+        "title": "",
+        "description": "",
+        "pricePerMonth": "",
+        "pricePerSquareMeterYear": ""
+    },
+    "address": {
+        "country": "",
+        "city": "",
+        "region": "",
+        "street": ""
+    },
+    "photo_link": [
+
+    ],
+    "details": {
+
+    }
+}
+
+# структура файла с гео информацией
+rentavik_object_geo = {
+    "id": "",
+    "latitude": "",
+    "longitude": ""
+}
+
+
+
+
+for pageNumber in pbar(range(1, 191 + 1)):
+
+    url = f"https://www.rentavik.ru/catalog/i/?PAGEN_1={pageNumber}"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    for tag in soup.find_all('script'):
+        objectINMapSuspect = str(tag.string).strip()
+        try:
+            if objectINMapSuspect[0:11] == 'objectInMap':
+                try:
+                    # получение jsonА
+                    building_string = objectINMapSuspect[20:].strip()
+                    building_string = building_string.replace("'", '"')
+                    building_string = building_string.replace('\n', ' ')
+                    json_building = json.loads(building_string)
+
+                    site_id = str(uuid4())
+                    site_id = site_id.replace('-', "")
+                    rentavik_object_data['id'] = f'{rentavik_id}_{site_id}'
+                    rentavik_object_geo['id'] = f'{rentavik_id}_{site_id}'
+                    rentavik_object_link = 'https://www.rentavik.ru' + \
+                        json_building['link']
+                    rentavik_object_data['resource']['link'] = rentavik_object_link
+                    try:
+                        latitude = json_building['geo'].split(',')[0]
+                        longtitude = json_building['geo'].split(',')[1]
+                        rentavik_object_geo['latitude'] = latitude
+                        rentavik_object_geo['longitude'] = longtitude
+                    except BaseException:
+                        pass
+                    rentavik_object_data['realEstate']['title'] = json_building['name']
+
+                    rentavik_object_data['address']['country'] = 'Россия'
+                    for address_piece in json_building['address'].split(','):
+                        if address_piece.startswith(" г.") or address_piece.startswith("г.") \
+                                or address_piece.startswith("г") or address_piece.startswith(" г"):
+                            rentavik_object_data['address']['city'] = address_piece
+
+                        if address_piece.startswith(" ул.") or address_piece.startswith("ул.")\
+                                or address_piece.startswith("ул") or address_piece.startswith(" ул"):
+                            rentavik_object_data['address']['street'] = address_piece
+
+                        if address_piece.endswith('область'):
+                            rentavik_object_data['address']['region'] = address_piece
+
+                    filename = rentavik_object_link.split(
+                        "/")[-1] if len(rentavik_object_link.split("/")[-1]) != 0 else rentavik_object_link.split("/")[-2]
+
+                    print(f"Работа с объектом ${filename}")
+                    # записываем геолокацию и больше не трогаем
+                    with open(f'./result/GEO_{filename}.json', 'w', encoding='utf-8') as fp:
+                        # записываем в json_data  json с геолокацией 
+                        json_data = json.dumps(
+                            rentavik_object_geo, ensure_ascii=False, indent=4)
+                        fp.write(json_data)
+
+                    # записываем в файл основной - прогодится во время выполнения функции
+                    with open(f'./result/{filename}.json', 'w', encoding='utf-8') as fp:
+                        # записываем в json_data значения основного json
+                        json_data = json.dumps(
+                            rentavik_object_data, ensure_ascii=False, indent=4)
+
+                        fp.write(json_data)
+                        # передаем имя файла. С помощью него отследим файл
+                        # после парсинга отдельной страницы запишем доп инфу о нем
+                        parsePage(filename)
+
+                except Exception as e:
+                    pass
+
+        except BaseException:
+            pass
